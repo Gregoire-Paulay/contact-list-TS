75f8ded81e0300e03a83e22fbcdb23464cb88d00
Grégoire Paulay <gregoirepaulay.pro@gmail;com>
2023-10-03 17:32:50 +0200

    Fonctionnel

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-// import { useState } from "react";
 import "./App.css";
 import { ContactList } from "./components/contactList";
 
 function App(): JSX.Element {
-  const handleChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-    set: React.Dispatch<React.SetStateAction<string>>
-  ) => {
-    // console.log(event.target.value);
-    set(event.target.value);
-  };
-
   return (
-    <main>
+    <>
       <div className="container">
         <h1>Contacts</h1>
-        <ContactList handleChange={handleChange} />
+        <ContactList />
       </div>
-    </main>
+    </>
   );
 }
 
--- src/components/contactList.tsx
+++ src/components/contactList.tsx
@@ -1,59 +1,81 @@
 import { useState } from "react";
-interface Props {
-  handleChange: (
-    event: React.ChangeEvent<HTMLInputElement>,
-    set: React.Dispatch<React.SetStateAction<string>>
-  ) => void;
-}
+
+const handleChange = (
+  event: React.ChangeEvent<HTMLInputElement>,
+  set: React.Dispatch<React.SetStateAction<string>>
+) => {
+  set(event.target.value);
+};
 interface MyContactList {
   userName: string;
   userMail?: string;
+  id: string;
 }
 
-export const ContactList = (props: Props): JSX.Element => {
-  const [ContactList, setContactList] = useState<Array<MyContactList>>([]);
+const randomId = () => {
+  return Math.random().toString(36).substring(7);
+};
+
+export const ContactList = (): JSX.Element => {
+  const [ContactList, setContactList] = useState<Array<MyContactList>>([
+    { userName: "Gregoire", userMail: "greg@mail.com", id: "12" },
+    { userName: "Rytlock", userMail: "rytlock@mail.com", id: "123" },
+    { userName: "Eir", id: "124" },
+  ]);
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
+
     if (name) {
       const listCopy = [...ContactList];
-      listCopy.push({ userName: name, userMail: email });
-
+      listCopy.push({ userName: name, userMail: email, id: randomId() });
       setContactList(listCopy);
-      console.log("ContactList ==>", ContactList);
     } else {
-      alert("Veuillez remplir le champ pour créer une nouvelle tâche.");
+      alert("Veuillez renseignez le nom pour ajouter un contact.");
     }
+
+    // console.log("ContactList ==>", ContactList);
+  };
+
+  const handleDelete = (index: any) => {
+    const listCopy = [...ContactList];
+    listCopy.splice(index, 1);
+    setContactList(listCopy);
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      {ContactList?.map((contact, index) => {
-        // console.log(contact);
+      <div className="contactList">
+        {ContactList?.map((contact, index) => {
+          return (
+            <div key={contact.id} className="contact">
+              <div>
+                <h2>{contact.userName}</h2>
+                <p>{contact.userMail}</p>
+              </div>
+              <button onClick={() => handleDelete(index)}>Delete</button>
+            </div>
+          );
+        })}
+      </div>
 
-        return (
-          <div key={index} className="contactList">
-            <p>{contact.userName}</p>
-            <p>{contact.userMail}</p>
-            <button>Delete</button>
-          </div>
-        );
-      })}
-      <input
-        type="text"
-        placeholder="name"
-        onChange={(e) => props.handleChange(e, setName)}
-        value={name}
-      />
-      <input
-        type="text"
-        placeholder="email"
-        onChange={(e) => props.handleChange(e, setEmail)}
-        value={email}
-      />
-      <input type="submit" value="Add Contact" />
+      <div className="inputUser">
+        <input
+          type="text"
+          placeholder="name"
+          onChange={(e) => handleChange(e, setName)}
+          value={name}
+        />
+        <input
+          type="text"
+          placeholder="email"
+          onChange={(e) => handleChange(e, setEmail)}
+          value={email}
+        />
+        <input type="submit" value="Add Contact" className="addList" />
+      </div>
     </form>
   );
 };
